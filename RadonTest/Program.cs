9204a36ddbc17db0a8c049e39ed15e3f1a9dd45b
Ron Tseytlin <ronts390@gmail.com>
2021-02-07 13:42:01 +0200

    added test: 9-slice sprite

--- RadonTest/Program.cs
+++ RadonTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SFML.Graphics;
 using SFML.Window;
+using Radon;
 using System;
 
 namespace RadonTest
@@ -9,6 +10,29 @@ namespace RadonTest
         static void Main(string[] args)
         {
             RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Radon Test");
+            window.SetVerticalSyncEnabled(true);
+
+            NineSliceInfo btnSliceInfo = new NineSliceInfo
+            {
+                texture = new Texture("btn.png"),
+
+                x0 = 0,
+                x1 = 5,
+                x2 = 11,
+                x3 = 16,
+
+                y0 = 0,
+                y1 = 5,
+                y2 = 11,
+                y3 = 16,
+            };
+
+            NineSliceSprite btn = new NineSliceSprite(btnSliceInfo)
+            {
+                position = new SFML.System.Vector2f(100, 200),
+                Width = 300,
+                Height = 100
+            };
 
             window.Closed += delegate
             {
@@ -22,11 +46,18 @@ namespace RadonTest
                 window.SetView(currentView);
             };
 
+            window.MouseMoved += (object obj, MouseMoveEventArgs args) =>
+            {
+                btn.Width = args.X - btn.position.X;
+                btn.Height = args.Y - btn.position.Y;
+            };
+
             while (window.IsOpen)
             {
                 window.DispatchEvents();
 
-                window.Clear();
+                window.Clear(new Color(120, 120, 120));
+                window.Draw(btn);
                 window.Display();
             }
         }
